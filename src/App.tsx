de89791ae76ad280aa43b01d1031201143319100
Pavel Chekushkin <x.chekushkin.pavel@gmail.com>
2021-06-12 08:14:09 +0400

    test dynamic-pagination

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface PhotosInterface {
+    albumId: number,
+    id: number,
+    title: string,
+    url: string,
+    thumbnailUrl: string
 }
+
+const App: React.FC = (): React.ReactElement => {
+    const [photos, setPhotos] = useState<PhotosInterface[]>([])
+    const [currentPage, setCurrentPage] = useState<number>(1)
+    const [fetching, setFetching] = useState<boolean>(true)
+    const [totalCount, setTotalCount] = useState<number>(0)
+
+    useEffect(() => {
+        if (fetching) {
+            axios.get(`https://jsonplaceholder.typicode.com/photos?_limit=10&_page=${currentPage}`).then((response) => {
+                setPhotos([...photos, ...response.data])
+                setCurrentPage(prevState => prevState + 1)
+                setTotalCount(response.headers['x-total-count'])
+            }).finally(() => setFetching(false))
+        }
+    }, [fetching])
+
+    useEffect(() => {
+        // @ts-ignore
+        document.addEventListener('scroll', scrollHandler)
+
+        return function () {
+            // @ts-ignore
+            document.removeEventListener('scroll', scrollHandler)
+        }
+    }, [])
+
+    const scrollHandler = (e: any) => {
+        if (e.target.documentElement.scrollHeight - (e.target.documentElement.scrollTop + window.innerHeight) < 100 && photos.length < totalCount) {
+            setFetching(true)
+        }
+
+    }
+
+    return (
+        <div className={'app'}>
+            {
+                photos && photos.map((photo) => {
+                    return (
+                        <div key={photo.id} className={'photo'}>
+                            <div className={'title'}>{photo.id}. {photo.title}</div>
+                            <img src={photo.thumbnailUrl} alt={photo.title}/>
+                        </div>
+                    )
+                })
+            }
+        </div>
+    );
+};
 
 export default App;
